8568901856ff0ffad3e0cf2d1063aa6d5ddf077c
Taylor Zhang <taylor.zhang@vanderbilt.edu>
2017-05-20 18:22:46 -0500

    rewrote game.py to be a module

--- bot.py
+++ bot.py
@@ -1,123 +1,63 @@
 import discord
 import asyncio
 import logging
-from game import Game
 from discord.ext import commands
 import traceback
 import sys
 import yaml
-log = logging.getLogger()
-BOARD_GAME = None
 stream = open('config.yaml')
 data = yaml.load(stream)
 print(data)
-client = discord.Client()
+#client = discord.Client()
 
 description = """beep boop I'm a bot that lets you play chess"""
-try:
-    import uvloop
-except ImportError:
-    pass
-else:
-    asyncio.set_event_loop_policy(uvloop.Event(LoopPolicy()))
 
-extensions = []
-discord_logger = logging.getLogger('discord')
-discord_logger.setLevel(logging.CRITICAL)
-log.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='tz.log', encoding='utf-8', mode='w')
-log.addHandler(handler)
+extensions = ['cogs.game']
+#discord_logger = logging.getLogger('discord')
+#discord_logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
+#handler = logging.FileHandler(filename='tz.log', encoding='utf-8', mode='w')
+#log.addHandler(handler)
 help_attrs = dict(hidden=True)
 prefix = ['?', '!', '\N{HEAVY EXCLAMATION MARK SYMBOL}']
 bot = commands.Bot(command_prefix=prefix, description=description, pm_help=None, help_attrs=help_attrs)
 print(commands.Group)
 
-
-
-@client.event
+@bot.event
 async def on_ready():
-    print('logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print('logged in as: ')
+    print(bot.user.name)
+    print(bot.user.id)
+    logging.warning('hello')
     print('-----')
-
-@client.event
+@bot.event
 async def on_message(message):
-    global BOARD_GAME
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-    elif message.content.startswith('!playchess'):
-        if BOARD_GAME != None:
-            await client.send_message(message.channel, 'Game already started')
-        else:
-            m = message.content.split(' ')
-            if len(m) == 2:
-                print('fuck')
-                print(m)
-                challengee = m[1].strip()
-                print(challengee)
-                if not challengee.startswith('<@'):
-                    await client.send_message(message.channel, 'You must tag another user with @ in order to play with them') 
-                else:    
-                    challenger = message.author.mention
-                    BOARD_GAME = Game(challenger, challengee)
-                    BOARD_GAME.Get_Picture("white")
-                    await client.send_file(message.channel, fp = 'output.png')
-                    #await client.send_message(message.channel, BOARD_GAME.board) 
-                    await client.send_message(message.channel, 'Your move, {}'.format(BOARD_GAME.white.username))
-                    #startgame 
-    elif BOARD_GAME != None and message.content.startswith('!play'):
-        move = message.content.split(' ')[1].strip()
-        if message.author.mention == BOARD_GAME.white.username:
-            if BOARD_GAME.white.turn:
-                try:
-                    BOARD_GAME.board.push_san(move)
-                    BOARD_GAME.white.turn = False
-                    BOARD_GAME.black.turn = True
-                    BOARD_GAME.Get_Picture("black")
-                    if BOARD_GAME.board.is_game_over() == True:
-                        await client.send_file(message.channel, fp = 'output.png')
-                        await client.send_message(message.channel, 'Game over. {}'.format(BOARD_GAME.board.result()))
-                        BOARD_GAME = None
-                    else:
-                        await client.send_file(message.channel, fp = 'output.png')
-                        #await client.send_message(message.channel, BOARD_GAME.board) 
-                        await client.send_message(message.channel, 'Your move, {}'.format(BOARD_GAME.black.username))
-                except ValueError:
-                    await client.send_message(message.channel, '{} is an illegal move, {}'.format(move, message.author))
-            else: 
-                await client.send_message(message.channel, 'It is not your turn, {}'.format(message.author))
-        elif message.author.mention == BOARD_GAME.black.username:
-            if BOARD_GAME.black.turn: 
-                try:
-                    BOARD_GAME.board.push_san(move)
-                    BOARD_GAME.white.turn = True
-                    BOARD_GAME.black.turn = False
-                    BOARD_GAME.Get_Picture("white")
-                    if BOARD_GAME.board.is_game_over() == True:
-                        await client.send_file(message.channel, fp = 'output.png')
-                        await client.send_message(message.channel, 'Game over. {}'.format(BOARD_GAME.board.result()))
-                        BOARD_GAME = None
-                    else:
-                        await client.send_file(message.channel, fp = 'output.png')
-                        #await client.send_message(message.channel, BOARD_GAME.board) 
-                        await client.send_message(message.channel, 'Your move, {}'.format(BOARD_GAME.white.username))
-                except ValueError:
-                    await client.send_message(message.channel, '{} is an illegal move, {}'.formate(move, message.author))
-            else: 
-                await client.send_message(message.channel, 'It is not your turn, {}'.format(message.author))
-        if BOARD_GAME.board.is_game_over() == true:
-            BOARD_GAME = None
-            await client.send_message(message.channel, 'Game over. {}'.format(BOARD_GAME.board.result()))
-        
-client.run(data['key'])
+    print('on_message')
+    if message.author.bot:
+        return
+    await bot.process_commands(message)
+@bot.command()
+async def load(extension_name : str):
+    try:
+        bot.load_extension(extension_name)
+    except (AttributeError, ImportError) as e:
+        await bot.say("'''py\n{}; {}\n'''".format(type(e).__name__, str(e)))
+        return
+    await bot.say("{} loaded.".format(extension_name))
 
+
+        
+if __name__ == '__main__':
+    for extension in extensions:
+        try:
+            logging.warning(extension)
+            bot.load_extension(extension)
+        except Exception as e:
+            print('Failed to load extension {}\n{}: {}'.format(extension, type(e).__name__,e))
+            logging.warning('Failed to load extension {}\n{}: {}'.format(extension, type(e).__name__,e))
+    bot.run(data['key'])
+    #handlers = log.handlers[:]
+    #for hdlr in handlers:
+        #hdlr.close()
+        #log.removeHandler(hdlr)
 
---
+++ cogs/game.py
@@ -0,0 +1,114 @@
+from discord.ext import commands
+import discord
+import chess
+import chess.svg
+import random
+import cairo
+import cairosvg
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+class Player:
+    def __init__(self, color, username):
+       self.color = color
+       self.username = username
+       self.turn = False
+    def move(self, move):
+        return
+    def print(self):
+        print(self.player)
+        print(self.username)
+        
+if __name__ == "__main__":
+    a = Player('white', 'adarsh')
+    a.print()
+
+class Game():
+    def __init__(self, bot):
+        self.board = chess.Board()
+        self.white = ''
+        self.black = ''
+        self.bot = bot
+    def Reset(self):
+        self.white = ''
+        self.black = ''
+        self.board = chess.Board()
+    def Get_Picture(self, color):
+        flipped = color == "black"
+        svg_data = chess.svg.board(self.board, flipped = flipped)
+        cairosvg.svg2png(bytestring=svg_data, write_to = "output.png")
+    def Take_Turn(self):
+        self.white.turn = not self.white.turn
+        self.black.turn = not self.black.turn
+    @commands.command()
+    async def test(self):
+        print("////")
+        await self.bot.say('test test')
+    @commands.command()
+    async def currentposition(self, color):
+        await self.bot.say('Current board position:')
+        Get_Picture(color)
+    @commands.command(pass_context=True)
+    async def playchess(self, ctx, name = ''):
+        if self.white != '' and self.black != '':
+            return await self.bot.say('There is already a game being played on this server')
+        if len(ctx.message.mentions) == 0:
+            return await self.bot.say('You must mention another player to start a game.')
+        if len(ctx.message.mentions) > 1:
+            return await self.bot.say('You are mentioning too many people')
+        if ctx.message.mentions[0] == ctx.message.author:
+            return await self.bot.say('You cannot play against yourself!')
+
+        author = ctx.message.author
+        opponent = ctx.message.mentions[0]
+        rand = random.randrange(0,2)
+        self.white = Player('white', author) if rand == 0 else Player('white', opponent.name + "#" +  opponent.discriminator)
+        self.black = Player('black', author) if rand != 0 else Player('black', opponent.name + "#" + opponent.discriminator)
+        self.white.turn = True
+        self.Get_Picture('white')
+        await self.bot.send_file(ctx.message.channel, fp = 'output.png')
+        await self.bot.send_message(ctx.message.channel, 'Your move, {}'.format(self.white.username))
+    @commands.command(pass_context=True)
+    async def move(self, ctx, move = ''):
+        if self.white == '' and self.black == '':
+            self.bot.say('There is no active game available')
+        if move == '':
+            self.bot.say('You must supply a move')
+        player = self.white if self.white.turn == True else self.black
+        logging.warning(player.username)
+        logging.warning(ctx.message.author)
+        if str(ctx.message.author) != str(player.username):
+            return await self.bot.send_message(ctx.message.channel, 'It is not your turn, {}'.format(ctx.message.author))
+        else:
+            try:
+                self.board.push_san(move)
+                self.Take_Turn()
+                color = "white" if player != self.white else "black"
+                nextuser = self.white.username if player != self.white else self.black.username
+                self.Get_Picture(color)
+                if self.board.is_game_over() == True:
+                    await self.bot.send_file(ctx.message.channel, fp = 'output.png')
+                    await self.bot.send_message(ctx.message.channel, 'Game over. {}'.format(self.board.result()))
+                    self.Reset()
+                else:
+                    await self.bot.send_file(ctx.message.channel, fp = 'output.png')
+                    await self.bot.send_message(ctx.message.channel, 'Your move, {}'.format(nextuser))
+            except ValueError:
+                await self.bot.send_message(ctx.message.channel, '{} is an illegal move, {}'.format(move, ctx.message.author))
+
+def setup(bot):
+    bot.add_cog(Game(bot))
+        
+if __name__ == "__main__":
+    chess1 = chess.Board()
+    chess1.push_san('e4')
+    svg_data = chess.svg.board(chess1, coordinates = False, flipped = False, style = chess.svg.DEFAULT_STYLE)
+    cairosvg.svg2png(bytestring=svg_data, write_to="output2.png")
+    chess2 = chess.Board()
+    #chess2.svg.board(chess2)
+    print(chess1)
+    print(chess2)
+    
+
+        
